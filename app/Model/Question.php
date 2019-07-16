c2ed7952c4e5df7e77ba347542ca5c067de4c571
kibuh <gkibuhguy@gmail.com>
2019-07-17 00:49:47 +0100

    CRUD for Question along with Question resource Transformation

--- app/Model/Question.php
+++ app/Model/Question.php
@@ -7,7 +7,17 @@ use App\User;
 
 class Question extends Model
 {
+
+
+    public function getRouteKeyName(){
+        return 'slug';
+    }
+
+    //protected $fillable=['title','slug','body','category_id','user_id'];
+    protected $guarded =[];
+    
     public function user(){
+
         return $this->belongsTo(User::class);
     }
 
@@ -17,5 +27,10 @@ class Question extends Model
 
     public function category(){
         return $this->belongsTo(Category::class);
+    }
+
+    public function getPathAttribute(){
+         return asset("api/question/$this->slug");
+        
     }
 }
