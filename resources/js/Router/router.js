04652b0beef202bc49c48ab83a38905616f6877c
kibuh <gkibuhguy@gmail.com>
2019-07-20 22:58:01 +0100

    Login component and login

--- resources/js/Router/router.js
+++ resources/js/Router/router.js
@@ -3,16 +3,19 @@ import VueRouter from 'vue-router'
 
 Vue.use(VueRouter)
 
-
+import Login from '../components/login/Login'
 
 const routes = [
-    // { path: '/login', component: Login },
+
+    { path: '/login', component: Login },
    
   ]
   
 
 const router = new VueRouter({
-    routes // short for `routes: routes`
+    routes, // short for `routes: routes`
+    hasbang: false,
+    mode:'history'
   })
 
 
